5f9b6694b6d22f53ba0eb7528dcf927ba9c1431a
Rashid Lasker <rashidlasker@gmail.com>
2018-10-31 14:56:21 -0400

    Add router functionality

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from '../../img/logo.svg';
 import './App.css';
+import Home from '../Home';
+import Projects from '../Projects';
+import Links from '../Links';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/projects/">Projects</Link>
+              </li>
+              <li>
+                <Link to="/links/">Links</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Home} />
+          <Route path="/projects/" component={Projects} />
+          <Route path="/links/" component={Links} />
+        </div>
+      </Router>
     );
   }
 }
