5294c7ceaffc4a5f54094a6371aa32d7eace7d84
Rashid Lasker <rashidlasker@gmail.com>
2018-11-07 14:05:16 -0500

    Create project search skeleton

--- src/views/Projects/Projects.js
+++ src/views/Projects/Projects.js
@@ -3,7 +3,29 @@ import projectData from '../../data/links.json';
 import './Projects.css';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  renderSearchBar() {
+    return (
+      <input type="text" className="projects-search" placeholder="Search.."/>
+    )
+  }
+
+  render() {
+    return (
+      <div className="projects-content">
+        {this.renderSearchBar()}
+      </div>
+    );
+  }
 }
 
 export default Projects;
