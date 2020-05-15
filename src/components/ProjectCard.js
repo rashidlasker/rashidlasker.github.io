e2b870742b3bd6c6c6c4152283815f059f85688e
Rashid Lasker <rashidlasker@gmail.com>
2020-05-15 01:11:21 -0400

    add tags

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const ProjectCard = ({ img, data, index }) => {
   const cardType = index % 4 + 1;
+  const tags = data['tags'].map(tag => (
+  <span className="card-tag">{tag.toUpperCase()}</span>
+  ));
   return (
     <div className={"card card-" + cardType}>
       <a href={data['projectLink']}>
@@ -10,9 +13,7 @@ const ProjectCard = ({ img, data, index }) => {
           <div className="card-title">{data['title']}</div>
           <div className="card-text">{data['summary']}</div>
           <div className="card-tags">
-            <span className="card-tag">WEB</span>
-            <span className="card-tag">PYTHON</span>
-            <span className="card-tag">JS</span>
+            {tags}
           </div>
         </div>
       </a>
