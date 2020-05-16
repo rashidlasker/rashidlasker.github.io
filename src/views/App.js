7ec07cdb72a09502341e4fda039452b59e1f5d43
Rashid Lasker <rashidlasker@gmail.com>
2020-05-15 23:16:34 -0400

    link to navlink

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, NavLink, Redirect } from "react-router-dom";
 import Home from './Home';
 import Projects from './Projects';
 import Project from './Project';
@@ -9,9 +9,9 @@ const App = () => {
     <Router>
       <div className="App">
         <nav className="content">
-          <Link className="link link-1" to="">Rashid Lasker</Link>
+          <NavLink className="link link-1" to="">Rashid Lasker</NavLink>
           <span className="divider">|</span>
-          <Link className="link link-3" to="/projects/">Projects</Link>
+          <NavLink className="link link-3" to="/projects/">Projects</NavLink>
           {/* <Link className="link link-4" to="/links/">Links</Link> */}
         </nav>
         <Switch>
